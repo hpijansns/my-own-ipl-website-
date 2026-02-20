1b69ef1f08aa488fcfebee1612052e71c9049781
Free <mohitrajpura9@gmail.com>
2026-02-20 15:03:52 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { 
+  getDatabase, 
+  ref, 
+  onValue, 
+  get, 
+  set, 
+  push, 
+  update, 
+  remove, 
+  query, 
+  orderByChild, 
+  equalTo,
+  serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAm5CDmBfpSL4tqbJLqUm1N4iqHyUCsZ6Y",
+  authDomain: "ticketarena-ipl.firebaseapp.com",
+  databaseURL: "https://ticketarena-ipl-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ticketarena-ipl",
+  storageBucket: "ticketarena-ipl.firebasestorage.app",
+  messagingSenderId: "460721412352",
+  appId: "1:460721412352:web:594c1fc55d4db48bdf3be5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { 
+  db, 
+  ref, 
+  onValue, 
+  get, 
+  set, 
+  push, 
+  update, 
+  remove, 
+  query, 
+  orderByChild, 
+  equalTo,
+  serverTimestamp 
+};
